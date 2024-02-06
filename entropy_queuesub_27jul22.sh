952c35192e2f01e9371aef915a3ea3ad6549052a
Amanda V. Meuser <74024369+amanda-meuser@users.noreply.github.com>
2024-02-06 12:15:34 -0500

    March 7th github version modified for current use

--- entropy_queuesub_27jul22.sh
+++ entropy_queuesub_27jul22.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-## Bash script for submitting a job to the SHARCNet Cedar queue, with starting values (ldak files)
-## Written by Amanda Meuser, adapted from Liz Mandeville, most recently modified March 2023
+## Bash script for submitting a job to the SHARCNet Cedar queue (modified back to use on Graham)
 ## Usage: sbatch entropy_queuesub_27jul22.sh mpgl_file k_number rep_number ldak_file (OR USE THE LOOPING SCRIPT)
-## sbatch /project/rrg-emandevi/hybrid_ameuser/entropy_queuesub_27jul22.sh /project/rrg-emandevi/hybrid_ameuser/EGM19_cc/starting_values_entropy/EGM19_target/EGM19_target_10mar23_miss0.5_mac3_Q30_DP3_ind75_maf001_CCCSBND.recode.mpgl 1 1 /project/rrg-emandevi/hybrid_ameuser/EGM19_cc/starting_values_entropy/EGM19_target/qk1inds.txt
-
-### DOUBLE CHECK STEPS AND Q SETTINGS!!!!!!! CURRENTLY SET TO 150K AND OFF
+## sbatch ../entropy_queuesub_27jul22.sh /project/def-emandevi/hybrid_ameuser_cedar_testing/EGM19_CCxBND/EGM19_target_27jul22_miss0.4_mac3_Q30_DP3_ind95_maf001_CCxBND.recode.mpgl 2 1 /project/def-emandevi/hybrid_ameuser_cedar_testing/EGM19_CCxBND/qk2inds.txt
 
 ### ---------- Job configuration --------------------------------------------
 
@@ -13,8 +10,8 @@
 # will be run on complete nodes NOT partial
 
 #SBATCH --nodes=1                       # number of nodes to use
-#SBATCH --time=00-03:00:00              # time (DD-HH:MM:SS)
-##SBATCH --time=00-02:00:00 		        # short time for testing
+#SBATCH --time=25-00:00:00              # time (DD-HH:MM:SS)
+##SBATCH --time=00-12:00:00 		        # short time for testing
 #SBATCH --account=rrg-emandevi          # account name
 #SBATCH --job-name="entropy_all"            # name to display in queue
 #SBATCH --ntasks-per-node=1             # taks per node (one core per node)
@@ -26,11 +23,7 @@
 
 # Load required modules - hdf5/1.8.18 gsl/2.5
 
-# normal modules
-module load nixpkgs/16.09  intel/2017.1  gsl/2.3  hdf5/1.8.18 # nixpkgs is now deprecated on cedar
-
-# modules for testing
-# module load StdEnv/2020 gcc/11.3.0 gsl/2.7 hdf5/1.12.2
+module load nixpkgs/16.09  intel/2017.1 gsl/2.3 hdf5/1.8.18
 
 ### ---------- Useful job infoformation -------------------------------------
 
@@ -88,11 +81,11 @@ k=$2
 rep=$3
 
 # MAKE SURE TO UPDATE BOTH 83 AND 85 IF CHANGING PARAMETERS
-entropyrun="$ENTROPY -i $IN_FILE -l 100000 -b 150000 -t 10 -k $k -o $PREFIX\_k$k\_150k_rep$rep\_$SUFFIX.hdf5 -m 1 -n 2 -w 0 -q $LDAK_FILE -Q 0 -r $RANDOM" # this line is printing our parameters to the log file
+entropyrun="$ENTROPY -i $IN_FILE -l 150000 -b 125000 -t 25 -k $k -o $PREFIX\_k$k\_150k_rep$rep\_$SUFFIX.hdf5 -n 2 -m 1 -w 0 -q $LDAK_FILE -Q 0 -r $RANDOM" # this line is printing our parameters to the log file
 echo $entropyrun
-$ENTROPY -i $IN_FILE -l 100000 -b 150000 -t 10 -k $k -o $PREFIX\_k$k\_150k_rep$rep\_$SUFFIX.hdf5 -m 1 -n 2 -w 0 -q $LDAK_FILE -Q 0 -r $RANDOM # this is telling the computer what to do 
+$ENTROPY -i $IN_FILE -l 150000 -b 125000 -t 25 -k $k -o $PREFIX\_k$k\_150k_rep$rep\_$SUFFIX.hdf5 -n 2 -m 1 -w 0 -q $LDAK_FILE -Q 0 -r $RANDOM # this is telling the computer what to do 
 
-echo "entropy run done. Results in "$PREFIX"_150k_rep"$rep"_$SUFFIX.hdf5"
+echo "entropy run done. Results in '$PREFIX'_150k_rep'$rep'_$SUFFIX.hdf5"
 
 
 
