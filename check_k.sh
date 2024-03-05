ad4cc1a31baf040c69ffcedca562cfce4e9de877
Amanda V. Meuser, MSc <74024369+amanda-meuser@users.noreply.github.com>
2024-03-05 14:58:22 -0500

    Slightly modified regex for entropy files

--- check_k.sh
+++ check_k.sh
@@ -3,18 +3,20 @@
 ## script to check model fit for entropy outputs with different values of k, outputs results to text file
 ## Amanda Meuser -- March 2023
 
-## USAGE: ./check_k.sh nametag /path/to/entropy/files (no / at the end of the path)
+## USAGE: check_k.sh nametag /path/to/entropy/files (no / at the end of the path)
+## TIP: for rerunning the script, delete the previous output files bc the script appends, so it'll add in duplicate lines of DIC values
 
 out=$1
 path=$2
 
-for file in $path/*.hdf5 
+# you can add text in here to differentiate b/w entropy runs
+for file in $path/*100k*.hdf5 
 do 
     /project/rrg-emandevi/bin/estpost.entropy -s 3 -p deviance $file >> $out"_DIC_raw.txt"
 done
 
 # remove file extension, retain k and rep values -- HARDCODED
-sed -e 's/file = \/project\/rrg-emandevi\/hybrid_ameuser\/AMP22\/entropy\/AMP22_target_03may23_miss0.6_mac3_Q30_DP3_ind95_maf001_//g' -e 's/_150k_/\t/g' -e 's/_qk[0-9]*inds\.hdf5//g' $out"_DIC_raw.txt" > temp.txt
+sed -e 's/file = \/project\/rrg-emandevi\/hybrid_ameuser\/AMP22_pub\/geographical_groupings\/algonquin\/entropy\/AMP22_Pimephales_algonquin_11jul23_miss0.5_mac3_Q30_DP3_maf001_ind95_maf001_//g' -e 's/_100k_/\t/g' -e 's/_qk[0-9]*inds\.hdf5//g' $out"_DIC_raw.txt" > temp.txt
 
 #remove unecessary repeated line
 sed -i '/parameter dimensions for/d' temp.txt
